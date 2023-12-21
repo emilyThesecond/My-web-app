1652b26b08e185396359c26156d0d7472e671258
emilyTheseond <esegundo02@gmail.com>
2023-12-21 12:21:52 -0600

    define glaze schema

--- models/glaze.js
+++ models/glaze.js
@@ -2,3 +2,20 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
+const glazeSchema = new Schema({
+    brand: {
+        type: String
+    },
+    color: {
+        type: String
+    },
+    itemNumber: {
+        Type: Number
+    },
+    style : {
+        type: string,
+        enum: ['Opaque', 'Gloss', 'Transparent']
+    }
+}, {
+    timestamps: true
+})
