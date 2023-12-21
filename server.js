3ba1357ae260e2abfca97b387348d8933a069f2b
emilyTheseond <esegundo02@gmail.com>
2023-12-21 09:52:52 -0600

    configure passport

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session')
+var passport = require('passport')
+
+
 require('dotenv').config();
 require('./config/database')
+require('./config/passport')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -27,6 +31,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
